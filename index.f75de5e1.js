ec3aabd731cdcc27b82b20191f804258c688a44b
Koshmak Nataliia <koshmaknatali@gmail.com>
2024-08-13 19:44:45 -0600

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,13 @@
+"use strict";
+const data = [
+    ...document.getElementsByClassName("population")
+];
+let total = 0;
+data.forEach((population)=>{
+    total += +population.textContent.replace(/,/g, "");
+});
+const average = total / data.length;
+document.querySelector(".average-population").innerText = average.toLocaleString("en-US");
+document.querySelector(".total-population").innerText = total.toLocaleString("en-US");
+
+//# sourceMappingURL=index.f75de5e1.js.map
